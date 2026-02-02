e50279607dbadbacb062e139b6c83b27117400ae
Jerome Paulos <jeromepaulos@icloud.com>
2026-02-02 16:57:54 -0500

    Cast orientation to an integer

--- src/Drivers/Concerns/GetsOrientationFromExif.php
+++ src/Drivers/Concerns/GetsOrientationFromExif.php
@@ -6,14 +6,14 @@ use Spatie\Image\Enums\Orientation;
 
 trait GetsOrientationFromExif
 {
-    /** @param  array{'Orientation'?: int}  $exif */
+    /** @param  array{'Orientation'?: int|string}  $exif */
     public function getOrientationFromExif(array $exif): Orientation
     {
         if (! isset($exif['Orientation'])) {
             return Orientation::Rotate0;
         }
 
-        return match ($exif['Orientation']) {
+        return match ((int) $exif['Orientation']) {
             3 => Orientation::Rotate180,
             6 => Orientation::Rotate270,
             8 => Orientation::Rotate90,
